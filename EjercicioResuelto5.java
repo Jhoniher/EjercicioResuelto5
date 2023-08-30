9de7d95046c4e437ad3cb0f36a2ef7da4d68b7ca
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 16:44:07 -0500

    Ejercicio Finalizado

--- EjercicioResuelto5.java
+++ EjercicioResuelto5.java
@@ -7,15 +7,15 @@ public class EjercicioResuelto5 {
 
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        double suma, x, y; 
+        double suma, x, y; //Declaracion de variables.
         suma=0;
         System.out.println("Ingrese el valor de X: ");
-        x=entrada.nextInt(); 
+        x=entrada.nextInt(); //Se ingresa X.
         suma=suma+x;
         System.out.println("Ingrese el valor de Y: ");
-        y=entrada.nextInt();
+        y=entrada.nextInt(); //Se ingresa Y.
         x=x+Math.pow(y,2);
-        suma=suma+(x/y);
+        suma=suma+(x/y); //Valor final de X.
         System.out.println("EL VALOR DE LA SUMA ES: "+suma);
     }
     
